7f557e61c71924e03e0ccb729f4c3ea6008193f4
Adam Cox <adam@adamcox.net>
2024-02-10 18:37:48 -0500

    routes

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,13 @@
 import { Routes } from '@angular/router';
+import { NavComponent } from './component/demo/nav/nav.component';
+import { StartPageComponent } from './page/start-page/start-page.component';
+import { DemoPageComponent } from './page/demo-page/demo-page.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  //{ path: '', redirectTo: 'home', pathMatch: 'full' },
+  //{ path: 'home', loadChildren: './home/home.module#HomeModule' },
+  //{ path: 'about', loadChildren: './about/about.module#AboutModule' }
+  { path: 'demo/nav', component: NavComponent },
+  { path: 'start', component: StartPageComponent },
+  { path: 'demo', component: DemoPageComponent },
+];

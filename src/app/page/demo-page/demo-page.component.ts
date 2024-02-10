57706c584ef6648db449df56b35696e324a12c58
Adam Cox <adam@adamcox.net>
2024-02-10 18:37:01 -0500

    demo page

---
+++ src/app/page/demo-page/demo-page.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-demo-page',
+  standalone: true,
+  imports: [],
+  templateUrl: './demo-page.component.html',
+  styleUrl: './demo-page.component.scss'
+})
+export class DemoPageComponent {
+
+}

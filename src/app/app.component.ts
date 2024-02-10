d27203e4c679e1cace9292108e39c4b02f5b18b8
Adam Cox <adam@adamcox.net>
2024-02-10 18:37:39 -0500

    app

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,25 @@
-import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { Component, signal } from '@angular/core';
+import { RouterModule, RouterOutlet } from '@angular/router';
+import { NavComponent } from './component/demo/nav/nav.component';
+import { CommonModule } from '@angular/common';
+import { MatButton } from '@angular/material/button';
+
+interface UserDto {
+  token: string | undefined;
+}
+
+interface ViewModel {
+  user: UserDto | undefined;
+}
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, NavComponent, CommonModule, RouterModule, MatButton],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss',
 })
 export class AppComponent {
   title = 'demo-app5';
+  vm = signal({ user: { token: undefined } });
 }

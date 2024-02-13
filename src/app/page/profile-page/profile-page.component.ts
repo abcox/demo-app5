8ad03eb0b38e71237e4f6d24553eb8188ee64251
Adam Cox <adam@adamcox.net>
2024-02-13 12:05:07 -0500

    mock profile

--- src/app/page/profile-page/profile-page.component.ts
+++ src/app/page/profile-page/profile-page.component.ts
@@ -1,12 +1,27 @@
-import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { Component, signal } from '@angular/core';
+import { MatButtonModule } from '@angular/material/button';
+import { MatCardModule } from '@angular/material/card';
+import { RouterModule } from '@angular/router';
 
 @Component({
   selector: 'app-profile-page',
   standalone: true,
-  imports: [],
+  imports: [CommonModule, MatButtonModule, MatCardModule, RouterModule],
   templateUrl: './profile-page.component.html',
-  styleUrl: './profile-page.component.scss'
+  styleUrl: './profile-page.component.scss',
 })
 export class ProfilePageComponent {
+  profile = {
+    name: 'Jessie',
+    email: 'jdoe@gmail.com',
+    registerDate: '6/15/2023',
+  };
+  vm = signal({ profile: this.profile });
+}
 
+export interface ProfileModel {
+  name: string;
+  email: string;
+  registerDate: string;
 }

4c180ab7bdb0362b9cb2b8b1042c55396b5dbc3e
Adam Cox <adam@adamcox.net>
2024-02-13 12:18:42 -0500

    add meeting mock

--- src/app/page/meeting-page/meeting-page.component.ts
+++ src/app/page/meeting-page/meeting-page.component.ts
@@ -1,10 +1,29 @@
-import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { Component, signal } from '@angular/core';
+import { MatButtonModule } from '@angular/material/button';
+import { MatCardModule } from '@angular/material/card';
+import { RouterModule } from '@angular/router';
 
 @Component({
   selector: 'app-meeting-page',
   standalone: true,
-  imports: [],
+  imports: [CommonModule, MatButtonModule, MatCardModule, RouterModule],
   templateUrl: './meeting-page.component.html',
   styleUrl: './meeting-page.component.scss',
 })
-export class MeetingPageComponent {}
+export class MeetingPageComponent {
+  meeting = {
+    name: 'Meeting 1',
+    date: '6/15/2023',
+    time: '12:00 PM',
+    location: '123 Main St.',
+  };
+  vm = signal({ meeting: this.meeting });
+}
+
+export interface MeetingModel {
+  name: string;
+  date: string;
+  time: string;
+  location: string;
+}

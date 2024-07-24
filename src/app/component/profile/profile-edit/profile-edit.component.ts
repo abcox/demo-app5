40a42386bd0af32c368fb2464394da5cae0287b7
Adam Cox <adam@adamcox.net>
2024-07-24 01:36:51 -0400

    style + add reminder opt to profile pg

--- src/app/component/profile/profile-edit/profile-edit.component.ts
+++ src/app/component/profile/profile-edit/profile-edit.component.ts
@@ -5,13 +5,19 @@ import {
   UserStateService,
 } from '../../../service/user-state/user-state.service';
 import { CommonModule } from '@angular/common';
-import { FormControl, ReactiveFormsModule } from '@angular/forms';
+import {
+  FormBuilder,
+  FormControl,
+  FormGroup,
+  ReactiveFormsModule,
+} from '@angular/forms';
 import { MatFormFieldModule } from '@angular/material/form-field';
 import { MatInputModule } from '@angular/material/input';
 import { MatButtonModule } from '@angular/material/button';
 import { MatCardModule } from '@angular/material/card';
 import { MatIconModule } from '@angular/material/icon';
 import { MatTooltipModule } from '@angular/material/tooltip';
+import { MatSelectModule } from '@angular/material/select';
 
 @Component({
   selector: 'app-profile-edit',
@@ -25,6 +31,7 @@ import { MatTooltipModule } from '@angular/material/tooltip';
     MatFormFieldModule,
     MatIconModule,
     MatInputModule,
+    MatSelectModule,
     MatTooltipModule,
   ],
   templateUrl: './profile-edit.component.html',
@@ -42,9 +49,22 @@ export class ProfileEditComponent {
         'https://media.licdn.com/dms/image/C5603AQGTleQRTom6eA/profile-displayphoto-shrink_800_800/0/1516253410442?e=1713398400&v=beta&t=XxcOf8bI0L2feMdyD_-KNRAyDFAnPAjhRLWyciogik0',
     };
   });
-  emailControl = new FormControl(this.profile().email);
-  nameControl = new FormControl(this.profile().name);
+  fb = inject(FormBuilder);
+  form = this.fb.group({
+    email: this.profile().email,
+    name: this.profile().name,
+    registerDate: this.profile().registerDate,
+    photoUrl: this.profile().photoUrl,
+    reminder: ['Email'],
+  });
+  reminderOptions = ['Email', 'Text', 'Phone'];
   uploadImage() {
     console.log('uploadImage');
+  }
+  selectReminderOption(opt: string) {
+    console.warn('selectReminderMethodOption not implemented');
+  }
+  save() {
+    console.warn('save not implemented');
   }
 }

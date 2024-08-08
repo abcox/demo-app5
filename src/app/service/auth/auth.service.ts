9e23a9dcaa85c4a209cc0af88dcd399807fc34e9
Adam Cox <adam@adamcox.net>
2024-08-08 18:29:30 -0400

    fix auth svc so it caches token and ctor init token

--- src/app/service/auth/auth.service.ts
+++ src/app/service/auth/auth.service.ts
@@ -1,6 +1,6 @@
 import { Injectable, inject, signal } from '@angular/core';
 import { Router } from '@angular/router';
-import { catchError, map, tap } from 'rxjs/operators';
+import { catchError, finalize, map, tap } from 'rxjs/operators';
 import {
   AuthResponse,
   AuthResult,
@@ -9,6 +9,7 @@ import {
 } from '../../../backend-api/v1';
 import { TOKEN_KEY } from '../../app.config';
 import { of } from 'rxjs/internal/observable/of';
+import { BehaviorSubject, EMPTY } from 'rxjs';
 
 @Injectable({
   providedIn: 'root',
@@ -16,9 +17,40 @@ import { of } from 'rxjs/internal/observable/of';
 export class AuthService {
   private authService = inject(backendAuthService);
   private router = inject(Router);
+  private token: string | undefined;
   isAuthenticated = signal(false);
 
-  constructor() {}
+  constructor() {
+    this.initToken();
+  }
+
+  private clearToken() {
+    localStorage.removeItem(TOKEN_KEY);
+    this.isAuthenticated.set(false);
+  }
+
+  private getToken() {
+    const token = localStorage.getItem(TOKEN_KEY);
+    this.isAuthenticated.set(!!token);
+    if (!token) {
+      console.warn('Token not found (or empty) in local storage', token);
+      return;
+    }
+    return token;
+  }
+
+  private initToken() {
+    this.token = this.getToken();
+  }
+
+  private setToken(token: string) {
+    if (!token) {
+      console.warn('Attempted to set empty token. Use clearToken instead.');
+      return;
+    }
+    localStorage.setItem(TOKEN_KEY, token);
+    this.isAuthenticated.set(true);
+  }
 
   login(request: { email: string; password: string; twoFactorCode: string }) {
     return this.authService.login(request).pipe(
@@ -32,7 +64,7 @@ export class AuthService {
         };
         if (result.success) {
           if (response.token) {
-            localStorage.setItem(TOKEN_KEY, response.token);
+            this.setToken(response.token);
           } else {
             console.warn('No token returned from login request', response);
           }
@@ -44,7 +76,7 @@ export class AuthService {
       catchError(err => {
         // todo: create shared error handling service - where we can log errors and show error messages
         console.error('Login err', err);
-        this.isAuthenticated.set(false);
+        this.clearToken();
         return of(<AuthResponse>{ success: false }); // todo: review why the square-brackets notation passes lint check: [{ success: false }]
       })
     );
@@ -70,7 +102,6 @@ export class AuthService {
       catchError(err => {
         // todo: create shared error handling service - where we can log errors and show error messages
         console.error('Error registering', err);
-        this.isAuthenticated.set(false);
         return of(<AuthResponse>{ success: false });
       })
     );
@@ -96,30 +127,33 @@ export class AuthService {
       catchError(err => {
         // todo: create shared error handling service - where we can log errors and show error messages
         console.error('Error registering', err);
-        this.isAuthenticated.set(false);
         return of(<AuthResponse>{ success: false });
       })
     );
   }
 
   logout() {
-    this.authService.logout().subscribe({
-      next: () => {
-        localStorage.removeItem('token');
-        this.isAuthenticated.set(false);
-        this.router.navigate(['/login']);
-      },
-      error: err => {
-        console.error('Error logging out', err);
-        this.isAuthenticated.set(false);
-        this.router.navigate(['/login']);
-      },
-    });
+    this.authService
+      .logout()
+      .pipe(
+        tap(response => {
+          console.log('Logout response', response);
+        }),
+        catchError(err => {
+          console.error('Error logging out', err);
+          return EMPTY;
+        }),
+        finalize(() => {
+          this.clearToken();
+          this.router.navigate(['/login']);
+        })
+      )
+      .subscribe();
   }
 
   // review the intention of this method
-  checkAuthStatus() {
+  /* checkAuthStatus() {
     const token = localStorage.getItem('token');
     this.isAuthenticated.set(!!token);
-  }
+  } */
 }
